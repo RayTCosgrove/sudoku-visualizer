2ac5552d3f0a6cad6cb303fae71e33b345118142
Raymond Cosgrove <raytcosgrove@gmail.com>
2019-09-02 14:55:35 -0400

    Fixed timing issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 const App = () => {
-   const [sudoku, setSudoku] = useState([
+  const [sudoku, setSudoku] = useState([
     [0, 0, 0, 2, 4, 5, 6, 0, 7],
     [0, 0, 0, 0, 0, 0, 0, 2, 0],
     [0, 0, 9, 0, 0, 0, 3, 0, 8],
@@ -54,7 +54,7 @@ const App = () => {
       [0, 8, 0, 0, 0, 0, 0, 0, 0],
       [7, 0, 2, 1, 8, 4, 0, 0, 0]
     ];
-  }
+  };
 
   const containsInRow = (row, num) => {
     return table[row].includes(num);
@@ -84,37 +84,36 @@ const App = () => {
   };
 
   const isAllowed = (row, col, num) => {
-
-    return !(containsInRow(row,num) || containsInCol(col,num) || containsInBlock(row,col,num));
-
+    return !(
+      containsInRow(row, num) ||
+      containsInCol(col, num) ||
+      containsInBlock(row, col, num)
+    );
   };
 
   const solveSudoku = () => {
     console.log("it ran");
-    for(let row = 0; row< 9; row++){
-      for(let col = 0; col <9; col++){
-        if(table[row][col]===0){
-          for(let testNum = 1; testNum<=9; testNum++){
-            if(isAllowed(row,col,testNum)){
-              
-              
-             table[row][col] = testNum;
-             pastTables.push([
-              [...table[0]],
-              [...table[1]],
-              [...table[2]],
-              [...table[3]],
-              [...table[4]],
-              [...table[5]],
-              [...table[6]],
-              [...table[7]],
-              [...table[8]]])
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (table[row][col] === 0) {
+          for (let testNum = 1; testNum <= 9; testNum++) {
+            if (isAllowed(row, col, testNum)) {
+              table[row][col] = testNum;
+              pastTables.push([
+                [...table[0]],
+                [...table[1]],
+                [...table[2]],
+                [...table[3]],
+                [...table[4]],
+                [...table[5]],
+                [...table[6]],
+                [...table[7]],
+                [...table[8]]
+              ]);
               console.log(pastTables.length);
-              if(solveSudoku()){
+              if (solveSudoku()) {
                 return true;
-                
-              }
-              else{
+              } else {
                 table[row][col] = 0;
                 pastTables.push([
                   [...table[0]],
@@ -125,9 +124,9 @@ const App = () => {
                   [...table[5]],
                   [...table[6]],
                   [...table[7]],
-                  [...table[8]]])
-                  console.log(pastTables.length);
-                
+                  [...table[8]]
+                ]);
+                console.log(pastTables.length);
               }
             }
           }
@@ -136,15 +135,28 @@ const App = () => {
       }
     }
     return true;
-  }
+  };
+
+  const onSolve = () => {
+    solveSudoku();
+
+    let id = setInterval(() => {
+      console.log("half second");
+      setSudoku(pastTables[currentTable]);
+      currentTable += 1;
+      if (currentTable === pastTables.length) {
+        clearInterval(id);
+      }
+    }, 50);
+  };
 
   return (
     <div>
       <main>
         <ul>
-          {sudoku.map((row,indexOuter) =>
-            row.map((val,indexInner) => (
-              <li key={indexOuter +"," + indexInner}>
+          {sudoku.map((row, indexOuter) =>
+            row.map((val, indexInner) => (
+              <li key={indexOuter + "," + indexInner}>
                 <span>{val === 0 ? " " : val}</span>
               </li>
             ))
@@ -156,18 +168,10 @@ const App = () => {
           to use this app.
         </div>
       </main>
-      <div style={{textAlign:'center'}}>
-        <button onClick={() => {
-          solveSudoku();
-          for(let i = 0; i< pastTables.length; i++){
-          setTimeout(() => {
-            setSudoku(pastTables[currentTable]);
-            currentTable+=1;
-          },75)
-          }
-        }}>Visualize!</button>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={onSolve}>Visualize!</button>
         <button onClick={reset}>Reset!</button>
-        </div>
+      </div>
     </div>
   );
 };
